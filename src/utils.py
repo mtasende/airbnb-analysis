94607840444850133d39a9828f6b655fb21c46bd
Miguel Tasende <miguel.tasende@gmail.com>
2018-10-16 14:44:11 -0300

    analysis finished

--- src/utils.py
+++ src/utils.py
@@ -43,3 +43,20 @@ def show_data(data):
     print('The data has shape: {}\n'.format(data.shape))
     print('There is {} missing data!\n'.format(data.isnull().sum().sum()))
     print(data.head())
+
+
+def common_values(series1, series2):
+    """ Shows the differences, intersections and union of two sets. """
+    values1 = set(series1)
+    values2 = set(series2)
+    intersection = set.intersection(values1, values2)
+    no_values2 = values1 - values2
+    no_values1 = values2 - values1
+    total = set.union(values1, values2)
+
+    print('Intersection: {}'.format(len(intersection)))
+    print('Total set 1: {}'.format(len(values1)))
+    print('Not in set 2: {}'.format(len(no_values2)))
+    print('Total set 2: {}'.format(len(values2)))
+    print('Not in set 1: {}'.format(len(no_values1)))
+    print('Total: {}'.format(len(total)))
